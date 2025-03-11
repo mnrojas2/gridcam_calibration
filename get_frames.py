0228c3629cdecc6d1d0e6329510b8420248f9f0e
mnrojas2 <mnrojas2@uc.cl>
2025-03-11 17:09:53 -0300

    Minor changes.

--- get_frames.py
+++ get_frames.py
@@ -7,16 +7,6 @@ import numpy as np
 from tqdm import tqdm
 
 
-# Initialize parser
-parser = argparse.ArgumentParser(description='Extracts frames from a specified video.')
-parser.add_argument('vidname', type=str, help='Name of video (mp4 format).')
-parser.add_argument('-o', '--output', type=str, metavar='folder', default='', help='Name of the folder that will contain the frames. If it is empty, the name of the file will be considered instead.')
-parser.add_argument('-rd', '--reduction', type=float, metavar='N', default=1, help='Reduction of number of frames (total/N).')
-parser.add_argument('-rs', '--residue', type=float, metavar='N', default=0, help='Residue or offset for the reduced number of frames.')
-parser.add_argument('-sn', '--startnumber', type=int, default=0, help="Number associated with the first frame and from where the count is starting. eg: 'frame0', 'frame1250'.")
-parser.add_argument('-af', '--alpha', type=float, default=1.0, help="Alpha value for change of Contrast of the images.")
-parser.add_argument('-bt', '--beta', type=float, default=0.0, help="Beta value for change of Contrast and Brightness of the images.")
-
 def cb_balance(img, alpha=1.0, beta=0.0, auto=False):
     # Adjust contrast and brightness balance of the image
     if auto:
@@ -25,10 +15,9 @@ def cb_balance(img, alpha=1.0, beta=0.0, auto=False):
         beta = -np.min(img)*alpha
     return alpha * img + beta
 
+
 # Main function
 def main():
-    # Take all arguments from terminal
-    args = parser.parse_args()
     print(f'Getting frames from {args.vidname}')
     
     try:
@@ -74,4 +63,21 @@ def main():
     except:
         print(f'No video was found in {args.vidname}')
 
-if __name__ == '__main__': main()
+
+
+if __name__ == '__main__': 
+    # Initialize parser
+    parser = argparse.ArgumentParser(description='Extracts frames from a specified video.')
+    parser.add_argument('vidname', type=str, help='Directory of video (mp4 format).')
+    parser.add_argument('-o', '--output', type=str, metavar='folder', default='', help='Name of the folder that will contain the frames. If it is empty, the name of the file will be considered instead.')
+    parser.add_argument('-rd', '--reduction', type=float, metavar='N', default=1, help='Reduction of number of frames (total/N).')
+    parser.add_argument('-rs', '--residue', type=float, metavar='N', default=0, help='Residue or offset for the reduced number of frames.')
+    parser.add_argument('-sn', '--startnumber', type=int, default=0, help="Number associated with the first frame and from where the count is starting. eg: 'frame0', 'frame1250'.")
+    parser.add_argument('-af', '--alpha', type=float, default=1.0, help="Alpha value for change of Contrast of the images.")
+    parser.add_argument('-bt', '--beta', type=float, default=0.0, help="Beta value for change of Contrast and Brightness of the images.")
+    
+    # Get parser arguments
+    args = parser.parse_args()
+    
+    # Main
+    main()
